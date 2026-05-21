24eaec7fffe3d3deb4a20a74c17949cabcc80cba
Muhammed Emre Eğer <m.emre.eger@gmail.com>
2026-05-22 02:49:39 +0300

    feat: implement discard pile service

---
+++ src/services/discardPileService/DiscardPileService.java
@@ -0,0 +1,65 @@
+package services.discardPileService;
+
+import abst.IDiscardPileService;
+import model.card.Card;
+import model.deck.DiscardPile;
+
+import java.util.List;
+
+
+public class DiscardPileService implements IDiscardPileService {
+    private static DiscardPileService instance;
+
+    private DiscardPileService() {
+    }
+
+    public static DiscardPileService getInstance() {
+
+        if (instance == null) {
+            instance = new DiscardPileService();
+        }
+
+        return instance;
+    }
+
+    //IHandService handService = HandService.getInstance();
+
+    private final DiscardPile discardPile = new DiscardPile();
+
+    /*
+    @Override
+    public void addCard(Hand hand, int cardIndex) {
+        //List<Card> discardDeck = discardPile.getDiscardedCards();
+        //discardDeck.add()
+        //discardPile.getDiscardedCards().add(hand.getCards().remove(cardIndex));
+
+        //discardPile.getDiscardedCards().add(card);
+        ;
+    }
+
+
+     */
+    public void addCardByCard(Card card) {
+        discardPile.getDiscardedCards().add(card);
+    }
+
+
+    @Override
+    public int getDiscardedCardCount() {
+        return discardPile.getDiscardedCards().size();
+    }
+
+
+    @Override
+    public void showDiscardedCards() {
+        int discardListSize = discardPile.getDiscardedCards().size();
+        for (int i = 0; i < discardListSize; i++) {
+            System.out.println((i + 1) + ") " + discardPile.getDiscardedCards().get(i));
+        }
+    }
+
+    @Override
+    public List<Card> getDiscardedCards() {
+        return discardPile.getDiscardedCards();
+    }
+}
