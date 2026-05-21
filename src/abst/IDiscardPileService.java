6ab3d23c5ddf2ff712a32a6c1768abc25c63a37d
Muhammed Emre Eğer <m.emre.eger@gmail.com>
2026-05-22 02:47:27 +0300

    feat: implement discard pile service

--- src/abst/IDiscardPileService.java
+++ src/abst/IDiscardPileService.java
@@ -1,14 +1,20 @@
 package abst;
 
 import model.card.Card;
-import model.deck.DiscardPile;
-import model.hand.Hand;
+
 
 import java.util.List;
 
-public interface IDiscardPileService {
-    void addCard(DiscardPile discardPile, Hand hand, int cardIndex);
 
-    int getDiscardedCardCount(DiscardPile discardPile);
+public interface IDiscardPileService {
+    //void addCard(Hand hand, int cardIndex);
+
+    void addCardByCard(Card card);
+
+    int getDiscardedCardCount();
+
+    void showDiscardedCards();
+
+    List<Card> getDiscardedCards();
 
 }
