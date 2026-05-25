4fa1e85ef62fe2cda642f0b46dad4a6ed1509738
Muhammed Emre Eğer <m.emre.eger@gmail.com>
2026-05-26 02:56:37 +0300

    feat: created useSpecialCard method refactor : fixing magic numbers

--- src/abst/ISpecialCardService.java
+++ src/abst/ISpecialCardService.java
@@ -1,7 +1,14 @@
 package abst;
 
+import model.deck.Deck;
 import model.hand.Hand;
 
 public interface ISpecialCardService {
     int applyEffects(Hand hand, int currentScore);
+
+    void useSpecialCard(
+            Hand hand,
+            Deck deck,
+            int specialCardIndex
+    );
 }
